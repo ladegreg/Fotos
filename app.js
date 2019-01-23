4581751e8cfbdb2b0320853e0610e91a235c8870
ladegreg <grzegorzlade@gmail.com>
2019-01-23 23:42:43 +0100

    ok
    15 go ok

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-tgafb.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`
 )
 .then(() => {
-  app.listen(8000);
+  app.listen(8010);
 })
 .catch(err => {
   console.log(err);
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,8 +1,39 @@
 import React, {Component} from 'react';
 
+import Modal from '../components/Modal/Modal';
+import Backdrop from '../components/Backdrop/Backdrop';
+import './Events.css';
+
 class EventsPage extends Component {
+  state = {
+    creating: false
+  };
+
+  startCreateEventHandler = () => {
+    this.setState({creating: true});
+  }
+
+  modalConfirmHandler = () => {
+    this.setState({creating: false});
+  };
+
+  modalCancelHandler = () => {
+    this.setState({creating: false});
+  };
+
   render() {
-    return <h1>the events page </h1>;
+    return (
+      <React.Fragment>
+        {this.state.creating && <Backdrop />}
+        {this.state.creating && <Modal title="Add Event" canCancle canConfirm onCancel={this.modalCancelHandler} onConfirm={this.modalConfirmHandler}>
+          <p>Modal Content</p>
+        </Modal>}
+        <div className="event-control">
+          <p>Share your own Events!</p>
+          <button className="btn" onClick={this.startCreateEventHandler}>Create Event</button>
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
