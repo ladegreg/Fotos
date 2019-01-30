50abd938577a3fad65f20fe13c9857efa7d3a78c
ladegreg <grzegorzlade@gmail.com>
2019-01-30 22:08:41 +0100

    Updates

---
+++ precache-manifest.1219eda611841d181f1feb011562b4a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "583eb416f1ce4336616e",
+    "url": "/Fotos/static/js/runtime~main.583eb416.js"
+  },
+  {
+    "revision": "50751e11fac5f68b32eb",
+    "url": "/Fotos/static/js/main.50751e11.chunk.js"
+  },
+  {
+    "revision": "d3a026dc3c47c853be1a",
+    "url": "/Fotos/static/js/1.d3a026dc.chunk.js"
+  },
+  {
+    "revision": "50751e11fac5f68b32eb",
+    "url": "/Fotos/static/css/main.4512a998.chunk.css"
+  },
+  {
+    "revision": "b008739f7335ee215916ef92ccab24f6",
+    "url": "/Fotos/index.html"
+  }
+];
